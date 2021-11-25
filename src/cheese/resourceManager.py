b33b0a20de036befb016884457a7bd14c7ae3497
KubaBoi <jakubanderle@outlook.cz>
2021-11-25 17:49:33 +0100

    first commit

---
+++ src/cheese/resourceManager.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+class ResMan:
+
+    @staticmethod
+    def setPath(path):
+        ResMan.path = path
+
+    # return name of file from path
+    @staticmethod
+    def getFileName(path):
+        return os.path.basename(path)
+
+    # return relative path from
+    @staticmethod
+    def getRelativePathFrom(path, fromPath):
+        return path.replace(fromPath, "")
+
+    # remove / from start or end
+    @staticmethod
+    def removeSlash(path, start=True):
+        if (path[0] == "/" and start):
+            path = path[1:]
+        elif (path[-1] == "/" and not start):
+            path = path[:-1]
+        return path
+
+    # joins two path together
+    @staticmethod
+    def joinPath(path1, path2):
+        path1 = ResMan.removeSlash(path1, False)
+        path2 = ResMan.removeSlash(path2)
+        return f"{path1}/{path2}"
+
+    # joins array together
+    @staticmethod
+    def joinArray(array):
+        path = "/"
+        for p in array:
+            path = ResMan.joinPath(path, p)
+        return path
+
+    # root dir of project
+    @staticmethod
+    def root():
+        return ResMan.path
+
+    # all source codes of project
+    @staticmethod
+    def src():
+        return f"{ResMan.path}/src"
+
+    # python source codes dir
+    @staticmethod
+    def pythonSrc():
+        return f"{ResMan.src()}/python"
+
+    # cheese dir
+    # DO NOT EVEN THINK ABOUT USING THIS METHOD I DARE YOU
+    @staticmethod
+    def cheese():
+        return f"{ResMan.src()}/cheese"
+
+    # other resources of project
+    @staticmethod
+    def resources():
+        return f"{ResMan.src()}/resources"
+
+    # tests
+    @staticmethod
+    def tests():
+        return f"{ResMan.src()}/tests"
+
+    # dir from which CheeseFramework is able to serve files (index.html) 
+    @staticmethod
+    def web():
+        return f"{ResMan.resources()}/web"
+
+    # dir for error sites
+    @staticmethod
+    def error():
+        return f"{ResMan.web()}/errors"
