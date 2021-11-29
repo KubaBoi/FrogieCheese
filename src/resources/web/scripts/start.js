c1e9744824bb39e7695e48e63c520bc61bde286e
KubaBoi <jakubanderle@outlook.cz>
2021-11-29 23:33:01 +0100

    debug

---
+++ src/resources/web/scripts/start.js
@@ -0,0 +1,136 @@
+async function loginButton() {
+    loginResponse = await login();
+    if (loginResponse.ERROR == null) {
+        setCookie("username", document.getElementById("fname").value, 5);
+        setCookie("password", document.getElementById("fpass").value, 5);
+
+        connectedUser = loginResponse.USER;
+        token = loginResponse.TOKEN;
+
+        settName.innerHTML = connectedUser.USER_NAME;
+        settPic.setAttribute("src", "/pictures/" + connectedUser.PICTURE_ID + ".png")
+
+        document.querySelector("#loggedAs").innerHTML = connectedUser.USER_NAME; //debug
+        document.querySelector("#loggedAsImg").setAttribute("src", "/pictures/" + connectedUser.PICTURE_ID + ".png")
+        
+        chatsArray = await getChats(0);
+        if (chatsArray.ERROR == null) { 
+            chatsArray = chatsArray.CHATS;
+            chatIds = [];
+            for (var i = 0; i < chatsArray.length; i++) {
+                chatIds.push(chatsArray[i].ID);
+            }
+
+            var chatMessages = await getChatMessages(0, chatIds);
+            if (chatMessages.ERROR == null) {
+                for (var ci = 0; ci < chatMessages.CHATS.length; ci++) {
+                    var chat = chatMessages.CHATS[ci];
+                    var chatInArray = chatsArray[findChatIndexById(chat.CHAT_ID)];
+
+                    chatInArray.MESSAGES = chat;
+                }
+
+                showChat(chatIds[0]);
+                loadChats();
+                //window.setInterval(updateChats, 1000);
+                window.setInterval(searchUsers, 1000);
+            }
+        }
+    }
+}
+
+function enterInput() {
+    var key = window.event.keyCode;
+
+    if (key === 13) { //&& !window.event.shiftKey) {
+        sendMess();
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
+
+function userSettingsButton() {
+    document.getElementById("userSettingsScreen").style.animation = "userSettingsDown 0.5s forwards";
+}
+
+function userSettingsButtonBack() {
+    document.getElementById("userSettingsScreen").style.animation = "userSettingsUp 0.5s forwards";
+}
+
+function registerButton() {
+    document.getElementById("loginScreen").style.animation = "registerLogin 1s forwards";
+    document.getElementById("registerScreen").style.animation = "registerRegisterScreen 1s forwards";
+}
+
+async function register() {
+    var name = document.querySelector("#rname");
+    var nB = true;
+    var mail = document.querySelector("#rmail");
+    var mB = true;
+    var pass1 = document.querySelector("#rpass1");
+    var p1B = true;
+    var pass2 = document.querySelector("#rpass2");
+    var pass = true;
+
+    if (name.value == "") {
+        nB = false;
+        name.setAttribute("class", "badInput");
+    }
+    else {
+        name.setAttribute("class", "input");
+    }
+    if (mail.value == "") {
+        mB = false;
+        mail.setAttribute("class", "badInput");
+    }
+    else {
+        mail.setAttribute("class", "input");
+    }
+    if (pass1.value == "") {
+        p1B = false;
+        pass1.setAttribute("class", "badInput");
+    }
+    else {
+        pass1.setAttribute("class", "input");
+    }
+    if (pass1.value != pass2.value && p1B) {
+        pass = false;
+        pass1.setAttribute("class", "badInput");
+        pass2.setAttribute("class", "badInput");
+    }
+    else {
+        pass1.setAttribute("class", "input");
+        pass2.setAttribute("class", "input");
+    }
+
+    if (nB && mB && pass) {
+        var user = await createUser(name.value, pass1.value, mail.value);
+        if (user.ERROR == null) {
+            document.getElementById("loginScreen").style.animation = "registeredLogin 1s forwards";
+            document.getElementById("registerScreen").style.animation = "registeredRegisterScreen 1s forwards";    
+        }
+    }
+}
+
+function uploadPicture() {
+    let xhr = new XMLHttpRequest();
+
+    let picture = document.getElementById("file").files[0];
+    let formData = new FormData();
+    
+    formData.append("userId", connectedUser.ID)
+    formData.append("picture", picture);
+
+    xhr.onreadystatechange = state => { 
+        console.log(xhr.response); 
+        connectedUser.PICTURE_ID = JSON.parse(xhr.response).PICTURE_ID;
+        settPic.setAttribute("src", "/pictures/" + connectedUser.PICTURE_ID + ".png")
+    } // err handling
+    xhr.timeout = 5000;
+    xhr.open("POST", "/users/uploadProfilePicture"); 
+    xhr.send(formData);
+
+}
